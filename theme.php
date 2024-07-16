1152a7b33e6cc15ff64ab08d2b43dde4dac71cd3
Max <108716198+Peantz@users.noreply.github.com>
2024-07-16 08:52:40 +0200

    Update theme.php

--- theme.php
+++ theme.php
@@ -1,152 +1,89 @@
+<?php global $Wcms ?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <!-- Encoding, browser compatibility, viewport -->
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<!-- Encoding, browser compatibility, viewport -->
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Search Engine Optimization (SEO) -->
-    <meta name="title" content="<?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?>" />
-    <meta name="description" content="<?= $Wcms->page('description') ?>">
-    <meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
-    <meta property="og:url" content="<?= $Wcms->getCurrentPageUrl() ?>" />
-    <meta property="og:type" content="website" />
-    <meta property="og:site_name" content="<?= $Wcms->get('config', 'siteTitle') ?>" />
-    <meta property="og:title" content="<?= $Wcms->page('title') ?>" />
-    <meta name="twitter:title" content="<?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?>" />
-    <meta name="twitter:description" content="<?= $Wcms->page('description') ?>" />
+		<!-- Search Engine Optimization (SEO) -->
+		<meta name="title" content="<?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?>" />
+		<meta name="description" content="<?= $Wcms->page('description') ?>">
+		<meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
+		<meta property="og:url" content="<?= $Wcms->getCurrentPageUrl() ?>" />
+		<meta property="og:type" content="website" />
+		<meta property="og:site_name" content="<?= $Wcms->get('config', 'siteTitle') ?>" />
+		<meta property="og:title" content="<?= $Wcms->page('title') ?>" />
+		<meta name="twitter:title" content="<?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?>" />
+		<meta name="twitter:description" content="<?= $Wcms->page('description') ?>" />
 
-    <!-- Website and page title -->
-    <title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
+		<!-- Website and page title -->
+		<title>
+			<?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?>
+		</title>
 
-    <!-- Admin CSS -->
-    <?= $Wcms->css() ?>
+		<!-- Admin CSS -->
+		<?= $Wcms->css() ?>
 
-    <!-- External CSS Links -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&amp;display=swap">
+		<!-- External CSS Links -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
-    <!-- Theme CSS -->
-    <link rel="stylesheet" rel="preload" as="style" href="<?= $Wcms->asset('css/style.css') ?>">
+		<!-- Theme CSS -->
+		<link rel="stylesheet" rel="preload" as="style" href="<?= $Wcms->asset('css/style.css') ?>">
+	</head>
+	
+	<body>
+		<!-- Admin settings panel and alerts -->
+		<?= $Wcms->settings() ?>
 
-    <!-- Navbar CSS -->
-    <style>
-        body {
-            margin: 0;
-            font-family: Arial, Helvetica, sans-serif;
-        }
+		<?= $Wcms->alerts() ?>
 
-        .topnav {
-            overflow: hidden;
-            background-color: #333;
-        }
-
-        .topnav a {
-            float: left;
-            display: block;
-            color: #f2f2f2;
-            text-align: center;
-            padding: 14px 16px;
-            text-decoration: none;
-            font-size: 17px;
-        }
-
-        .topnav a:hover {
-            background-color: #ddd;
-            color: black;
-        }
-
-        .topnav a.active {
-            background-color: #04AA6D;
-            color: white;
-        }
-
-        .topnav .icon {
-            display: none;
-        }
-
-        @media screen and (max-width: 600px) {
-            .topnav a:not(:first-child) {display: none;}
-            .topnav a.icon {
-                float: right;
-                display: block;
-            }
-        }
-
-        @media screen and (max-width: 600px) {
-            .topnav.responsive {position: relative;}
-            .topnav.responsive .icon {
-                position: absolute;
-                right: 0;
-                top: 0;
-            }
-            .topnav.responsive a {
-                float: none;
-                display: block;
-                text-align: left;
-            }
-        }
-    </style>
-</head>
-<body>
-    <!-- Admin settings panel and alerts -->
-    <?= $Wcms->settings() ?>
-    <?= $Wcms->alerts() ?>
-
-    <!-- Navbar -->
-    <div class="topnav" id="myTopnav">
-        <a href="#home" class="active">Home</a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-            <i class="fa fa-bars"></i>
-        </a>
+<section id="topMenu">
+    <div class="inner">
+        <nav>
+            <ul class="menu">
+                <!-- Logo hinzufügen -->
+                <li class="logo">
+                    <a href="<?= $Wcms->url() ?>">
+                        <img src="<?= $Wcms->asset('img/logo.png') ?>" alt="Logo">
+                    </a>
+                </li>
+                <!-- Menü -->
+                <?= $Wcms->menu() ?>
+            </ul>
+        </nav>
     </div>
+</section>
 
-    <div style="padding-left:16px">
-        <h2>Responsive Topnav Example</h2>
-        <p>Resize the browser window to see how it works.</p>
-    </div>
 
-    <div id="wrapper">
-        <section id="intro" class="wrapper style1 fullscreen">
-            <div class="inner">
-                <!-- Main content for each page -->
-                <?= $Wcms->page('content') ?>
-            </div>
-        </section>
 
-        <section class="wrapper style2">
-            <div class="inner">
-                <!-- Static editable block, same on each page -->
-                <?= $Wcms->block('subside') ?>
-            </div>
-        </section>
-    </div>
+		<div id="wrapper">
+			<section id="intro" class="wrapper style1 fullscreen">
+				<div class="inner">
+					<!-- Main content for each page -->
+					<?= $Wcms->page('content') ?>
+				</div>
+			</section>
 
-    <footer class="wrapper style2">
-        <div class="inner">
-            <!-- Footer -->
-            <?= $Wcms->footer() ?>
-        </div>
-    </footer>
+			<section class="wrapper style2">
+				<div class="inner">
+					<!-- Static editable block, same on each page -->
+					<?= $Wcms->block('subside') ?>
+				</div>
+			</section>
+		</div>
 
-    <!-- Admin JavaScript. More JS libraries can be added below -->
-    <?= $Wcms->js() ?>
+		<footer class="wrapper style2">
+			<div class="inner">
+				<!-- Footer -->
+				<?= $Wcms->footer() ?>
+			</div>
+		</footer>
 
-    <!-- Navbar JavaScript -->
-    <script>
-        function myFunction() {
-            var x = document.getElementById("myTopnav");
-            if (x.className === "topnav") {
-                x.className += " responsive";
-            } else {
-                x.className = "topnav";
-            }
-        }
-    </script>
-</body>
+		<!-- Admin JavaScript. More JS libraries can be added below -->
+		<?= $Wcms->js() ?>
+	</body>
 </html>
