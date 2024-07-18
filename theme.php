bc8f8888fb48f3587d857cf04860e79be8f8b9ee
Max <108716198+Peantz@users.noreply.github.com>
2024-07-18 09:29:46 +0200

    Update theme.php

--- theme.php
+++ theme.php
@@ -3,10 +3,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Encoding, browser compatibility, viewport -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
+
+    <!-- Search Engine Optimization (SEO) -->
     <meta name="title" content="<?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?>" />
     <meta name="description" content="<?= $Wcms->page('description') ?>">
     <meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
@@ -16,12 +18,24 @@
     <meta property="og:title" content="<?= $Wcms->page('title') ?>" />
     <meta name="twitter:title" content="<?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?>" />
     <meta name="twitter:description" content="<?= $Wcms->page('description') ?>" />
-    
+
+    <!-- Website and page title -->
+    <title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
+
+    <!-- Admin CSS -->
+    <?= $Wcms->css() ?>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>">
+
+    <!-- ICON
+    <link rel="icon" href="http://example.com/favicon.png">
+     -->
 </head>
+
 <body>
+    <!-- Admin settings panel and alerts -->
     <?= $Wcms->settings() ?>
     <?= $Wcms->alerts() ?>
 
@@ -43,22 +57,33 @@
         </div>
     </section>
 
-    <div id="wrapper" class="container mt-5">
-        <section id="intro" class="py-5">
-            <div class="inner text-center">
+    <div id="wrapper">
+        <section id="intro" class="wrapper style1 fullscreen">
+            <div class="inner">
+                <!-- Main content for each page -->
                 <?= $Wcms->page('content') ?>
             </div>
         </section>
+
+        <!-- Static editable block, same on each page -->
+        <!--<section class="wrapper style2">
+            <div class="inner">
+
+                <<?= $Wcms->block('subside') ?>
+            </div>
+        </section>-->
     </div>
 
-    <footer class="bg-dark text-white py-4">
-        <div class="container text-center">
+    <footer class="wrapper style2">
+        <div class="inner">
+            <!-- Footer -->
             <?= $Wcms->footer() ?>
         </div>
     </footer>
 
+    <!-- Admin JavaScript. More JS libraries can be added below -->
     <?= $Wcms->js() ?>
+
+    <!-- Bootstrap JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
-</body>
-</html>
