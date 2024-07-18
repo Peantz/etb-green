36b36d8f7f23e70445ca6dfabb61b07962ea60d0
Max <108716198+Peantz@users.noreply.github.com>
2024-07-18 07:44:08 +0200

    Update theme.php

--- theme.php
+++ theme.php
@@ -28,6 +28,7 @@
     <!-- External CSS Links -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    include_once 'path/to/your/functions.php';
 
     <!-- Theme CSS -->
     <link rel="stylesheet" rel="preload" as="style" href="<?= $Wcms->asset('css/style.css') ?>">
